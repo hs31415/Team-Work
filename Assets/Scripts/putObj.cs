10ca835647c2b2238928ce7c16ba7be7ba90d340
Shuo Huang <128951219+hs31415@users.noreply.github.com>
2023-11-16 20:53:49 +0800

    将实例化的单位挂载到canvas下以实现ui显示

--- Assets/Scripts/putObj.cs
+++ Assets/Scripts/putObj.cs
@@ -5,6 +5,7 @@ public class SpawnAndMove : MonoBehaviour
     public GameObject prefab; // 预设体
     public Transform target; // 目标位置
     public TextMeshProUGUI textObject; // 通过在编辑器中拖动来设置Text对象
+    public Canvas canvas; // Canvas对象的引用
     public int Cost;
     public void SetPrefab(GameObject newPrefab)
     {
@@ -40,6 +41,7 @@ public class SpawnAndMove : MonoBehaviour
                     spawnPosition.z = 0; // 确保 z 轴为 0
 
                     GameObject newObject = Instantiate(prefab, spawnPosition, Quaternion.identity); // 生成预设体
+                    newObject.transform.SetParent(canvas.transform, false);
 
                     CostSystem costSystem = textObject.GetComponent<CostSystem>();
                     costSystem.currentCost -= objectState.cost;
