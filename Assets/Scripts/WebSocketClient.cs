8449294dd5fff4bcee404b6617dbe5cad8a805fc
durianc <durian_cwy@163.com>
2023-11-24 16:31:52 +0800

    修改websocketClient

--- Assets/Scripts/WebSocketClient.cs
+++ Assets/Scripts/WebSocketClient.cs
@@ -1,17 +1,18 @@
-using System.IO;
-using System.Net.WebSockets;
 using UnityEngine;
-using UnityEngine.Networking.PlayerConnection;
 using WebSocketSharp;
 
-public class WebSocketClient : MonoBehaviour
+public class WebSocketClientTest : MonoBehaviour
 {
-    private WebSocketSharp.WebSocket ws;
+    private WebSocket ws;
+    private string roomID = "1"; // 默认房间号为1，您可以根据需要进行修改
 
-    private void Start()
+    private void Awake()
     {
+        // 根据输入的房间号构建WebSocket连接地址
+        string url = "ws://10.133.18.99:60001"; // 假设玩家标识为"player1"
+
         // 创建WebSocket连接
-        ws = new WebSocketSharp.WebSocket("ws://your-server-address");
+        ws = new WebSocket(url);
 
         // 注册事件处理程序
         ws.OnOpen += OnWebSocketOpen;
@@ -22,7 +23,6 @@ public class WebSocketClient : MonoBehaviour
         // 启动连接
         ws.Connect();
     }
-
     private void OnDestroy()
     {
         // 关闭WebSocket连接
@@ -40,32 +40,15 @@ public class WebSocketClient : MonoBehaviour
         ws.Send("Hello server!");
     }
 
-    private void OnWebSocketMessage(object sender, WebSocketSharp.MessageEventArgs e)
+    private void OnWebSocketMessage(object sender, MessageEventArgs e)
     {
         Debug.Log("Received message from server: " + e.Data);
 
         // 处理从服务器接收的消息
         // 根据消息内容执行相应的逻辑
-
-        // 解析接收到的JSON数据
-        MyDataObject cardData = JsonUtility.FromJson<MyDataObject>(e.Data);
-
-        // 使用解析后的卡牌数据执行相关操作
-        string name = cardData.name;
-        string type = cardData.type;
-        float attack = float.Parse(cardData.attack);
-        float defense = float.Parse(cardData.defense);
-        int attackRange = int.Parse(cardData.attack_range);
-        int moveSpeed = int.Parse(cardData.move_speed);
-        int cost = int.Parse(cardData.cost);
-        string description = cardData.description;
-
-        // 执行根据卡牌数据执行的逻辑
-        // ...
-
     }
 
-    private void OnWebSocketError(object sender, WebSocketSharp.ErrorEventArgs e)
+    private void OnWebSocketError(object sender, ErrorEventArgs e)
     {
         Debug.LogError("WebSocket error: " + e.Message);
     }
@@ -73,18 +56,5 @@ public class WebSocketClient : MonoBehaviour
     private void OnWebSocketClose(object sender, CloseEventArgs e)
     {
         Debug.Log("WebSocket connection closed with code: " + e.Code);
-    }
-
-    [System.Serializable]
-    public class MyDataObject
-    {
-        public string name;
-        public string type;
-        public string attack;
-        public string defense;
-        public string attack_range;
-        public string move_speed;
-        public string cost;
-        public string description;
     }
 }
